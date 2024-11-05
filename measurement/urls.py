91516bfe96274f54edd9678e177b490dea9bb0ca
Denis Buzin <buzin.den@bk.ru>
2024-11-05 20:18:57 +0300

    Day 2

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from measurement.views import demo
+from measurement.views import AllSensorsView, SensorView, MeasurementView
 
 urlpatterns = [
-    path('demo/', demo)
+    path('sensors/', AllSensorsView.as_view()),
+    path('sensors/<pk>/', SensorView.as_view()),
+    path('measurements/', MeasurementView.as_view())
 ]
